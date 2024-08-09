e5857f5746864143af36c09ea3ea86d318f04ef9
Zen Wang <zxc82990@gmail.com>
2024-08-09 17:05:39 +0800

    feat: add web3Auth get wallet via jwt

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,43 @@ import Link from "next/link"
 import { signIn, signOut, useSession } from "next-auth/react"
 import styles from "./header.module.css"
 
+import { CHAIN_NAMESPACES, WEB3AUTH_NETWORK } from "@web3auth/base";
+import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
+// Import Single Factor Auth SDK for no redirect flow
+import { Web3Auth, decodeToken } from "@web3auth/single-factor-auth";
+import { useEffect, useState } from "react";
+
+// RPC libraries for blockchain calls
+import RPC from "./evm.web3";
+// import RPC from "./evm.ethers";
+// import RPC from "./evm.viem";
+
+const verifier = "wld-id-login";
+
+const clientId = "BIDkIB_b8KGDnF1rj77nInYKlrmqk1yL2B9-xvfH2ngARHSeHxlst0Y97HgEpUe8Xg2GySwzUyO2l3EyP3lZWVE"; // get from https://dashboard.web3auth.io
+
+const chainConfig = {
+  chainId: "0x1",
+  displayName: "Ethereum Mainnet",
+  chainNamespace: CHAIN_NAMESPACES.EIP155,
+  tickerName: "Ethereum",
+  ticker: "ETH",
+  decimals: 18,
+  rpcTarget: "https://rpc.ankr.com/eth",
+  blockExplorerUrl: "https://etherscan.io",
+};
+
+const privateKeyProvider = new EthereumPrivateKeyProvider({
+  config: { chainConfig },
+});
+
+// Initialising Web3Auth Single Factor Auth SDK
+const web3authSfa = new Web3Auth({
+  clientId, // Get your Client ID from Web3Auth Dashboard
+  web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET, // ["cyan", "testnet"]
+  usePnPKey: false, // Setting this to true returns the same key as PnP Web SDK, By default, this SDK returns CoreKitKey.
+  privateKeyProvider,
+});
 // The approach used in this component shows how to build a sign in and sign out
 // component that works on pages which support both client and server side
 // rendering, and avoids any flash incorrect content on initial page load.
@@ -9,6 +46,58 @@ export default function Header() {
   const { data: session, status } = useSession()
   const loading = status === "loading"
 
+  const [isLoggingIn, setIsLoggingIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const getAccounts = async () => {
+    if (!web3authSfa.provider) {
+      console.log("No provider found");
+      return;
+    }
+    const rpc = new RPC(web3authSfa.provider);
+    const userAccount = await rpc.getAccounts();
+    console.log(userAccount);
+  };
+  useEffect(() => {
+    const init = async () => {
+      try {
+        web3authSfa.init();
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    init();
+  }, []);
+  useEffect(() => {
+    async function getWallet() {
+      try {
+        if (!web3authSfa) {
+          console.log("Web3Auth Single Factor Auth SDK not initialized yet");
+          return;
+        }
+        setIsLoggingIn(true);
+        const idTokenResult = decodeToken(session.accessToken)
+        console.log(idTokenResult)
+        await web3authSfa.connect({
+          verifier,
+          verifierId: (idTokenResult.payload as any).sub,
+          idToken: session.accessToken,
+        });
+        setIsLoggingIn(false);
+        setIsLoggedIn(true);
+      } catch (err) {
+        // Single Factor Auth SDK throws an error if the user has already enabled MFA
+        // One can use the Web3AuthNoModal SDK to handle this case
+        setIsLoggingIn(false);
+        signOut()
+        console.error(err);
+      }
+    }
+    console.log(session)
+    if (session?.accessToken && !isLoggedIn) {
+      getWallet()
+    }
+  }, [session]);
   return (
     <header>
       <noscript>
@@ -16,9 +105,8 @@ export default function Header() {
       </noscript>
       <div className={styles.signedInStatus}>
         <p
-          className={`nojs-show ${
-            !session && loading ? styles.loading : styles.loaded
-          }`}
+          className={`nojs-show ${!session && loading ? styles.loading : styles.loaded
+            }`}
         >
           {!session && (
             <>
@@ -28,9 +116,10 @@ export default function Header() {
               <a
                 href={`/api/auth/signin`}
                 className={styles.buttonPrimary}
-                onClick={(e) => {
+                onClick={async (e) => {
                   e.preventDefault()
-                  signIn("worldcoin") // when worldcoin is the only provider
+                  let res = await signIn("worldcoin") // when worldcoin is the only provider
+                  console.log(res)
                   // signIn() // when there are multiple providers
                 }}
               >
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -25,11 +25,20 @@ export const authOptions: NextAuthOptions = {
       },
     },
   ],
+  session: { strategy: "jwt" },
   callbacks: {
-    async jwt({ token }) {
-      token.userRole = "admin";
-      return token;
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token
+      }
+      return token
     },
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.accessToken = token.accessToken
+      return session
+    }
   },
   debug: true,
 };
